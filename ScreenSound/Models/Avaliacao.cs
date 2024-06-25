fd433efe6eef369830287071608f98884ac2545a
zaccheu <gu.zaccheu8@gmail.com>
2024-06-25 13:39:04 -0300

    aula 3

---
+++ ScreenSound/Models/Avaliacao.cs
@@ -0,0 +1,11 @@
+﻿namespace ScreenSound.Models;
+
+internal class Avaliacao //internal só o projeto poded enxergar
+{
+    public Avaliacao(int nota)
+    {
+        Nota = nota;
+    }
+
+    public int Nota { get; }
+}
